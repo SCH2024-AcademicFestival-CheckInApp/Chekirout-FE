83072ff270ac9c20562b8385d9bf495397736191
Monixc <chris13404@gmail.com>
2024-10-01 06:17:53 +0900

    chore: next.config.js pwa 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "next-pwa";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // (에러 핸들링) reactStrictMode 활성화
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      const workboxPluginIndex = config.plugins.findIndex(
+        (plugin) => plugin.constructor.name === "GenerateSW"
+      );
+
+      if (workboxPluginIndex !== -1) {
+        const [workboxPlugin] = config.plugins.splice(workboxPluginIndex, 1);
+        config.plugins.push(workboxPlugin);
+      }
+    }
+
+    return config;
+  },
+};
+
+export default withPWA({
+  dest: "public", // public 폴더에 파일 생성
+  disable: process.env.NODE_ENV === "development", // 개발자 모드에서 pwa 비활성화
+  register: true, // 서비스 워커 등록
+  skipWaiting: true,
+})(nextConfig);
