189d01adf5457372bb80de072a5319cd5732f779
Monixc <chris13404@gmail.com>
2024-10-01 22:45:27 +0900

    feat: 초기 화면 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Splash from "@/components/Splash";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen flex justify-center items-center text-center">
+    <main className="w-full h-screen flex flex-col justify-between items-center bg-white ">
       <Splash />
-      <div>웰컴</div>
+      <div className="text-center pt-[160px]">
+        <div className="text-lg text-[#235698]  mb-4">SCH SW•AI Festival</div>
+        <div className="text-5xl font-black text-[#235698]">체키라웃</div>
+      </div>
+      <div className="flex-grow flex justify-center items-center">
+        <Image src="/assets/icon512.png" alt="logo" width={240} height={240} />
+      </div>
+      <div className="flex flex-col space-y-4 py-8">
+        <Button className="w-[328px] h-11 bg-[#235698] text-white font-semibold rounded-lg">
+          로그인
+        </Button>
+        <Button className="w-[328px] h-11 bg-[#acacac] text-white font-semibold rounded-lg">
+          회원가입
+        </Button>
+      </div>
     </main>
   );
 }
