90495227e5cbb07ba90426fed952fcb66f7ddbbb
Monixc <chris13404@gmail.com>
2024-10-10 00:05:21 +0900

    fix: 밸류값 불일치로 인한 403 오류 해결

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -30,10 +30,11 @@ const FormSchema = z
 
 const departments = [
   { label: "컴퓨터소프트웨어공학과", value: "CSE" },
-  { label: "의료 IT공학과", value: "Medical_IT" },
-  { label: "정보보호학과", value: "InfoSec" },
+  { label: "의료 IT공학과", value: "MEDIT" },
+  { label: "정보보호학과", value: "IP" },
   { label: "사물인터넷학과", value: "IoT" },
-  { label: "메타버스게임학과", value: "Metaverse" },
+  { label: "메타버스게임학과", value: "METABUS" },
+  { label: "AI빅데이터공학과", value: "AI_BIGDATA" },
 ];
 
 export default function SignupPage() {
