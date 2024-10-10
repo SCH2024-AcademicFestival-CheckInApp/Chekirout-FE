cea129fa5acb4bea34fa4eae6d207cabe477523f
Antraxmin <antraxmin@naver.com>
2024-10-10 23:06:04 +0900

    feat: 전체 프로그램 일정 조회 페이지 생성

---
+++ src/components/ui/FloatingQRButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import { QrCode } from 'lucide-react'; 
+
+const FloatingQRButton: React.FC = () => {
+  return (
+    <Link href="#" passHref>
+      <div className="fixed bottom-0 right-0 z-50 w-full max-w-[480px] mx-auto left-0">
+        <div className="relative w-full h-full">
+          <button
+            className="absolute right-4 bottom-16 bg-[#235698] text-white rounded-full p-4 shadow-lg hover:bg-blue-800 transition-colors duration-300"
+            aria-label="QR 코드 스캔"
+          >
+            <QrCode size={28} />
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default FloatingQRButton;
