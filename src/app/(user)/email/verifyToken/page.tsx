7c456f72c637065891bc1f0a245c540a2b2c5e51
Chaemin-Lim (임채민) <antraxmin@naver.com>
2024-10-20 22:06:19 +0900

    fix: 이메일 인증 로직 server action으로 분리

--- src/app/(user)/email/verifyToken/page.tsx
+++ src/app/(user)/email/verifyToken/page.tsx
@@ -1,41 +1,39 @@
-"use client";
+"use client"
 
-import { useRouter } from 'next/navigation'; 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState, useTransition } from 'react';
+import { verifyEmail } from '@/app/actions/verifyEmail';
 
 export default function VerifyTokenPage() {
-  const router = useRouter();
   const [verificationStatus, setVerificationStatus] = useState<string | null>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const handleVerifyEmail = (token: string) => {
+    startTransition(async () => {
+      try {
+        const response = await verifyEmail(token);
+        setVerificationStatus('인증 성공');
+      } catch (error) {
+        setVerificationStatus('인증 실패');
+      }
+    });
+  };
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get('token');
-
     if (token) {
-      axios
-        .get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/verify-email?token=${token}`)
-        .then((response) => {
-          setVerificationStatus('인증 성공');
-          setTimeout(() => {
-            router.push('/signup?emailVerified=true');
-          }, 1000);
-        })
-        .catch((error) => {
-          console.error(error);
-          setVerificationStatus('인증 실패');
-        });
+      handleVerifyEmail(token);
     }
-  }, [router]);
+  }, []);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      {verificationStatus === '인증 성공' ? (
-        <p className="text-green-500 text-2xl">이메일 인증 성공! 회원가입 페이지로 이동합니다...</p>
-      ) : verificationStatus === '인증 실패' ? (
-        <p className="text-red-500 text-2xl">인증에 실패했습니다. 다시 시도해주세요.</p>
+    <div>
+      {isPending ? (
+        <p>이메일 인증 중입니다...</p>
+      ) : verificationStatus === '인증 성공' ? (
+        <p>이메일 인증이 성공했습니다!</p>
       ) : (
-        <p className="text-gray-500 text-2xl">이메일 인증 중입니다...</p>
+        <p>인증에 실패했습니다. 다시 시도해주세요.</p>
       )}
     </div>
   );
